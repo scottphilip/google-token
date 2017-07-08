8ad77bc0f43ce92a1c84b6c866bf3408bef9d007
Scott Philip <sp@scottphilip.com>
2017-07-08 20:13:07 +0200

    Setup

--- Tests/TestConfiguration.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from GoogleToken import GoogleTokenParameters
-
-
-class TestConfiguration(unittest.TestCase):
-
-    def test_create_instance_googletokenparameters(self):
-        instance = GoogleTokenParameters()
-        self.assertIsNotNone(instance, "GoogleTokenParameters instance is None")
