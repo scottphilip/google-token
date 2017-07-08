f26d6434a57eb2150c672705994f94942ac0ba44
Scott Philip <sp@scottphilip.com>
2017-07-08 20:18:30 +0200

    Setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='google-token',
+    name='GoogleToken',
     description='Google User Account login automation',
     url='http://github.com/scottphilip/google-token',
     author='Scott Philip',
     author_email='sp@scottphilip.com',
     packages=['GoogleToken'],
-    version='0.1',
+    version='0.1.1',
     install_requires=['selenium', 'pyotp'],
     license='GNU (v3) License',
     keywords=['Google Account', 'Access Token', 'oauth2', 'Automation'],
@@ -17,7 +17,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
