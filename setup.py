a53fe3d27254c4dc9e08880dfbeff992fc066123
Scott Philip <sp@scottphilip.com>
2017-07-25 21:29:47 +0200

    Build 0.6.1 - requests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Scott Philip',
     author_email='sp@scottphilip.com',
     packages=['GoogleToken'],
-    version='0.6',
+    version='0.6.1',
     install_requires=['selenium', 'pyotp', 'cryptography'],
     license='GNU (v3) License',
     keywords=['Google Account', 'Access Token', 'oauth2', 'Automation'],
